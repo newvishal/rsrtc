7ccd8f8f237bd2af6b6534519db828592599c5cd
VISHAL SINGH <vishalsingh1812@gmail.com>
2022-02-27 18:07:38 +0530

    section done

--- src/app/main/pages/section/add-edit/add-edit.component.ts
+++ src/app/main/pages/section/add-edit/add-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { Observable } from 'rxjs';
+import { SectionService } from 'src/app/services/section.service';
+
+import {ISection} from '../../../../shared/ts';
 
 @Component({
   selector: 'app-add-edit',
@@ -6,10 +13,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit.component.css']
 })
 export class AddEditComponent implements OnInit {
-
-  constructor() { }
+  SectionForm: FormGroup;
+  submitted = false;
+  bsubject: ISection;
+  constructor(private formBuilder: FormBuilder,public toastr: ToastrManager,private sectionService: SectionService,private _router: Router) { }
 
   ngOnInit(): void {
+    this.SectionForm = this.formBuilder.group({
+      sectionName: ['', Validators.required],
+      shortCode: ['', Validators.required],
+    });
+
+    this.patchLocalStorageData();
+  }
+
+  patchLocalStorageData() {
+    this.sectionService.subject.subscribe(res => {
+      if(typeof res == 'string') {
+         this.bsubject = JSON.parse(res);
+      } else {
+        this.bsubject = res;
+      }
+      this.SectionForm.patchValue(this.bsubject);
+    });
+  }
+  
+  get myForm() { return this.SectionForm.controls; }
+
+  addSection() {
+    this.submitted = true;
+    if (this.SectionForm.invalid) {
+      return;
+    } else {
+      if(this.bsubject.sectionId) {
+        this.sectionService.put({...this.SectionForm.value, sectionId: this.bsubject.sectionId} as ISection, this.bsubject.sectionId).subscribe({
+          next: res =>{
+            this._router.navigate(["dashboard/section/"]);
+            this.toastr.successToastr(res['message']);
+            localStorage.removeItem('details');
+            this.sectionService.saveDetails({sectionId: '', sectionName: "", shortCode: '', status: false })
+          },
+          error: err =>{
+            console.log(err);
+            this.toastr.warningToastr(err);
+          }
+        })
+        return
+      }
+      this.sectionService.add({...this.SectionForm.value} as ISection).subscribe({
+        next: res =>{
+          this._router.navigate(["dashboard/section/"]);
+          this.toastr.successToastr(res['message']);
+        },
+        error: err =>{
+          console.log(err);
+          this.toastr.warningToastr(err);
+        }
+      })
+    }
+
   }
 
 }
--- src/app/main/pages/section/list/list.component.ts
+++ src/app/main/pages/section/list/list.component.ts
@@ -1,18 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-declare var $:any;
+import { Component, OnInit, AfterViewInit, ViewChild  } from '@angular/core';
+import { SectionService } from 'src/app/services/section.service';
+
+import { MatPaginator } from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+import {ISection} from '../../../../shared/ts'
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  displayedColumns: string[] = ['sectionId', 'sectionName', 'shortCode', 'status','actions'];
+  dataSource: MatTableDataSource<ISection>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
-
+  constructor(public sectionService: SectionService, public route: Router) { }
+  loading: boolean = false;
   ngOnInit(): void {
-    $(function(e) {
-      $('#Sectionexample').DataTable();
-    } );
+    this.getSection();
+  }
+
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getSection(){
+    this.loading = true;
+    this.sectionService.find().subscribe((res: ISection[]) => {
+      const Section = res['data'] as ISection[];
+      this.dataSource = new MatTableDataSource(Section);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.loading = false;
+    }, (err) => {
+      console.log(err)
+    });
+
+  }
+
+  EditSection(detail: ISection) {
+    console.log(detail)
+    this.sectionService.saveDetails(detail);
+    this.route.navigate(['dashboard/section/addEditSection'])
   }
 
 }
